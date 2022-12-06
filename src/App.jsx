5f3ed2e9f82748215c992b3ac864bfc200fb6ef8
Josh <seapolco@gmail.com>
2022-12-06 18:26:51 +0000

    Replaced static content with static props.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import katie from './assets/katie.png';
+import star from './assets/Star.svg';
 
 
 /// DB //////
@@ -13,7 +15,11 @@ function App() {
         <>
         <Navbar />
         <Hero />
-        <Card/>
+        <Card avail="SOLD OUT" image={katie} star={star} 
+              ratingAvg="5.0"  ratingCount="(6) • USA"
+              cardDescr="Life lessons with Katie Zaferes"
+              cost="From $136"
+        />
         </>
     )
  
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,25 +5,25 @@ import star from '../assets/Star.svg';
 
 
 
-const Card = () => {
+const Card = (props) => {
 
     return(
        <div className="cardSection">
             <div className="card">
                 <div className="cardImageContainer">
-                    <div className="availability">SOLD OUT</div>
-                    <img src={swim} alt="" className="cardImage" />
+                    <div className="availability">{props.avail}</div>
+                    <img src={props.image} alt="" className="cardImage" />
                 </div>
                 <div className="cardText">
                     <div className="ratingCountyContainer">
                         <div className="starRating">
-                            <img src={star}  alt="" className="starIcon" />
-                            <p className="ratingAverage">5.0</p>
+                            <img src={props.star}  alt="" className="starIcon" />
+                            <p className="ratingAverage">{props.ratingAvg}</p>
                         </div>
-                        <p className="ratingCountCountry">(6) • USA </p>
+                        <p className="ratingCountCountry">{props.ratingCount}</p>
                     </div>
-                    <p className="cardDescription">Life lessons with Katie Zaferes</p>
-                    <p className="cost"><b>From $136</b> / person</p>
+                    <p className="cardDescription">{props.cardDescr}</p>
+                    <p className="cost"><b>{props.cost}</b> / person</p>
                 </div>
             </div>
        </div> 
@@ -31,3 +31,28 @@ const Card = () => {
 }
 
 export default Card
+
+
+
+
+// return(
+//     <div className="cardSection">
+//          <div className="card">
+//              <div className="cardImageContainer">
+//                  <div className="availability">SOLD OUT</div>
+//                  <img src={swim} alt="" className="cardImage" />
+//              </div>
+//              <div className="cardText">
+//                  <div className="ratingCountyContainer">
+//                      <div className="starRating">
+//                          <img src={star}  alt="" className="starIcon" />
+//                          <p className="ratingAverage">5.0</p>
+//                      </div>
+//                      <p className="ratingCountCountry">(6) • USA </p>
+//                  </div>
+//                  <p className="cardDescription">Life lessons with Katie Zaferes</p>
+//                  <p className="cost"><b>From $136</b> / person</p>
+//              </div>
+//          </div>
+//     </div> 
+//  )
